2435384507f18b71d47c4d0db639a1af08a9df29
F4k3r22 <fredyriveraacevedo13@gmail.com>
2025-02-19 22:00:50 -0600

    Se me habia olvidado implementar el Stream con OpenAI

--- Artemisa/Llm/providers/openai.py
+++ Artemisa/Llm/providers/openai.py
@@ -1,13 +1,14 @@
 from openai import OpenAI
 
 class OpenAIAgent:
-    def __init__(self, API_KEY, llm_model=None, max_tokens=None):
+    def __init__(self, API_KEY, llm_model=None, max_tokens=None, stream=False):
         self.api_key = API_KEY
         if not API_KEY:
             raise ValueError("API_KEY es requerida")
         
         self.llm_model = llm_model
         self.max_tokens = max_tokens
+        self.stream = stream
 
         if self.llm_model is None:
             self.llm_model = 'gpt-4o-mini'
@@ -16,6 +17,10 @@ class OpenAIAgent:
             self.max_tokens = 1500
     
     def query(self, query: str, system_prompt = None, format_response=False):
+
+        if self.stream:
+            return self.queryStream(query, system_prompt)
+
         query = query
         system_prompt = system_prompt
         client = OpenAI(api_key=self.api_key)
@@ -52,3 +57,29 @@ class OpenAIAgent:
             content = response.choices[0].message.content
             return content
         return None
+    
+    def queryStream(self, query: str, system_prompt = None):
+        query = query
+        system_prompt = system_prompt
+        client = OpenAI(api_key=self.api_key)
+        try:
+            messages = []
+            if system_prompt:
+                messages.append({"role": "system", "content": system_prompt})
+            messages.append({"role": "user", "content": query})
+
+            stream = client.chat.completions.create(
+                model=self.llm_model,
+                messages=messages,
+                max_tokens=self.max_tokens,
+                stream=True,
+                #stream_options={"include_usage": True},
+            )
+
+            for chunk in stream:
+                if chunk.choices[0].delta.content is not None:
+                    yield chunk.choices[0].delta.content
+
+        except Exception as e:
+            print(f"Error en el streaming: {str(e)}")
+            yield None
